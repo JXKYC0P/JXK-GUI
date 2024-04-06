e23d85cbba49753f8b7caf40749d8409fbd3dbd7
JXKYC0P <166175168+JXKYC0P@users.noreply.github.com>
2024-04-07 00:14:04 +0200

    Create GUI.lua

---
+++ GUI.lua
@@ -0,0 +1,9 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("JXKYHUB", "Synapse")
+
+--main
+local Tab = Window:NewTab("Main")
+local Section = Tab:NewSection("Player")
+Section:NewButton("Back/Front Flip", "Gymnastics lol", function()
+    
+end)
